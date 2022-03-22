84132857182ff494485be10f7f89c665b76fe3f2
levi <iooicoder@gmail.com>
2022-03-22 15:24:47 +0800

    feat: can export

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const config = {
+  authToken: process.env.NOTION_KEY,
+  databaseId: process.env.NOTION_DATABASE_ID,
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import { Client } from "@notionhq/client"
+import fs from "fs"
+import { Client } from "@notionhq/client";
+import { config } from "./config.js";
+import { NotionToMarkdown } from "notion-to-md";
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
 
-const databaseId = process.env.NOTION_DATABASE_ID
+const notion = new Client({ auth: config.authToken });
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
-async function addItem(text) {
+const getAllPages = async (databaseId) => {
   try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: { 
-          title:[
-            {
-              "text": {
-                "content": text
-              }
-            }
-          ]
-        }
-      },
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+    console.log(response);
+    return response.results;
   } catch (error) {
-    console.error(error.body)
+    console.error(error.body);
   }
-}
+};
 
-addItem("Yurts in Big Sur, California")
+const convertToMd = async (client, pageList) => {
+  for (let i = 0; i < pageList.length; i++) {
+    const page = pageList[i];
+    const pageId = page.id;
+    const pageTitle = page.properties.title.title[0].plain_text + ".md";
+
+    console.log(pageId);
+    const mdBlocks = await client.pageToMarkdown(pageId);
+    const mdString = client.toMarkdownString(mdBlocks);
+
+    fs.writeFile(pageTitle, mdString, (err) => {
+      console.log(err);
+    });
+  }
+  return;
+};
+
+const pageList = await getAllPages(config.databaseId);
+const resp = await convertToMd(n2m, pageList);
